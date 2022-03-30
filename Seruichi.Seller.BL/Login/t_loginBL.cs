887183c2617023510a6ee7cde3d193e9445d35bf
mya059-pc <mya059-pc@MYA059>
2022-03-30 12:34:31 +0630

    cread model ,BL,fonts and modify controller for login form

---
+++ Seruichi.Seller.BL/Login/t_loginBL.cs
@@ -0,0 +1,44 @@
+﻿using Models;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using Seruichi.Common;
+
+namespace Seruichi.BL
+{
+    public class t_loginBL
+    {
+        private CommonBL commonBL = new CommonBL();
+
+
+        public bool CheckPrefecturesByIdpsw(string TenStaffCD, string TenStaffPW, out string errorcd, out string outPrefCD)
+        {
+            errorcd = "";
+            outPrefCD = "";
+
+            var sqlParams = new SqlParameter[]
+            {
+                new SqlParameter("@TenStaffCD", SqlDbType.VarChar){ Value = TenStaffCD.ToStringOrNull() },
+                new SqlParameter("@TenStaffPW", SqlDbType.VarChar){ Value = TenStaffPW.ToStringOrNull() }
+            };
+
+            DBAccess db = new DBAccess();
+            var dt = db.SelectDatatable("pr_t_login_Select_PrefecturesByIdpsw", sqlParams);
+            if (dt.Rows.Count == 0)
+            {
+                errorcd = "E103"; //入力された値が正しくありません
+                return false;
+            }
+
+            var dr = dt.Rows[0];
+            if (string.IsNullOrEmpty(dr["RegionCD"].ToStringOrEmpty()))
+            {
+                errorcd = "E201"; //査定サービスの対象外地域です
+                return false;
+            }
+
+            outPrefCD = dr["PrefCD"].ToStringOrEmpty();
+            return true;
+        }
+    }
+}
