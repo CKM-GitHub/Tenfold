d4533c16c84ccdae5c7c9601cdb11951ca1c5103
nwemar <capital.nwemarwin@gmail.com>
2022-04-19 15:00:47 +0630

    NMW

--- Seruichi.Tenfold.Web/App_Start/RouteConfig.cs
+++ Seruichi.Tenfold.Web/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Seruichi.Tenfold.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "t_login", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "t_login", action = "Login", id = UrlParameter.Optional }
             );
         }
     }
