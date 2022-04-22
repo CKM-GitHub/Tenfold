0001aaccb7d8eb6c07b7af032351d0c5d4590ca0
capital-aymisoe <capital.aymisoe@gmail.com>
2022-04-22 13:29:59 +0630

    byami

--- Models/RealEstate/r_login/r_loginModel.cs
+++ Models/RealEstate/r_login/r_loginModel.cs
@@ -11,17 +11,10 @@ namespace Models.RealEstate.r_login
         public string RealECD { get; set; }
         public string REStaffCD { get; set; }
         public string REPassword { get; set; }
-    }
-    public class r_login_l_log_Model
-    {
-        public string LogDateTime { get; set; }
-        public byte LoginKBN { get; set; }
-        public string LoginID { get; set; }
-        public string RealECD { get; set; }
-        public string LoginName { get; set; }
-        public string IPAddress { get; set; }
-        public string PageID { get; set; }
-        public string ProcessKBN { get; set; }
-        public string Remarks { get; set; }
+        public string REStaffName { get; set; }
+        public byte PermissionChat { get; set; }
+        public byte PermissionSetting { get; set; }
+        public byte PermissionPlan { get; set; }
+        public byte PermissionInvoice { get; set; }
     }
 }
