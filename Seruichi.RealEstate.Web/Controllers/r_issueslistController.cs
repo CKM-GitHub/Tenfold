654cc6a53ee7903ba908df6e02786fa1f72a6991
Than Zaw Aung <capital.thanzawaung@gmail.com>
2022-04-29 09:38:02 +0630

    tza for r_issueslist

---
+++ Seruichi.RealEstate.Web/Controllers/r_issueslistController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Seruichi.RealEstate.Web.Controllers
+{
+    public class r_issueslistController : Controller
+    {
+        // GET: r_issueslist
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
