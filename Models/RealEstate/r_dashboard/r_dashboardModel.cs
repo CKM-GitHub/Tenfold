07b1e682ceda5105c8edaac29bf4ef5d5c172df6
Pann Nu Zan <Pann Nu Zan@DESKTOP-52825H4>
2022-05-03 17:37:48 +0630

    pnz

--- Models/RealEstate/r_dashboard/r_dashboardModel.cs
+++ Models/RealEstate/r_dashboard/r_dashboardModel.cs
@@ -3,21 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Models.RealEstate.r_login;
 
 namespace Models.RealEstate.r_dashboard
 {
-    public class r_dashboardModel
+    public class r_dashboardModel:r_loginModel
     {
-        public string RealECD { get; set; }
-        public string REStaffCD { get; set; }
-        public string REPassword { get; set; }
-        public string REStaffName { get; set; }
-        public byte PermissionChat { get; set; }
-        public byte PermissionSetting { get; set; }
-        public byte PermissionPlan { get; set; }
-        public byte PermissionInvoice { get; set; }
+        //public string RealECD { get; set; }
+        //public string REStaffCD { get; set; }
+        //public string REPassword { get; set; }
+        //public string REStaffName { get; set; }
+        //public byte PermissionChat { get; set; }
+        //public byte PermissionSetting { get; set; }
+        //public byte PermissionPlan { get; set; }
+        //public byte PermissionInvoice { get; set; }
         public DateTime ConfDateTime { get; set; }
-        public string Oldestdate { get; set; }
+        //public string Oldestdate { get; set; }
        
     }
 }
