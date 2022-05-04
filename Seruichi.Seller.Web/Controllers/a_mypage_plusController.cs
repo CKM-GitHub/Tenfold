1311dbf877ce81d61180adefcf3e570ac0fb7ab1
mya059-pc <mya059-pc@MYA059>
2022-05-04 21:24:11 +0630

    add by ct

---
+++ Seruichi.Seller.Web/Controllers/a_mypage_plusController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Models;
+
+namespace Seruichi.Seller.Web.Controllers
+{
+    public class a_mypage_plusController : BaseController
+    {
+        // GET: a_mypage_plus
+        public ActionResult Index()
+        {
+            LoginUser user = SessionAuthenticationHelper.GetUserFromSession();
+            if (!SessionAuthenticationHelper.ValidateUser(user))
+            {
+                return RedirectToAction("Index", "a_login");
+            }
+
+            return View();
+        }
+    }
+}
