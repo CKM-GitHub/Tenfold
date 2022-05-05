610fb969cafb827e63ebc2f000edacabf24ec034
Pann Nu Zan <Pann Nu Zan@DESKTOP-52825H4>
2022-05-05 10:05:42 +0630

    pnz

---
+++ Seruichi.BL/Database/StoredProcedure/pr_r_dashboard_SelectNumForNumberOfDecline.sql
@@ -0,0 +1,21 @@
+﻿IF EXISTS (select * from sys.objects where name = 'pr_r_dashboard_SelectNumForNumberOfDecline')
+BEGIN
+    DROP PROCEDURE [pr_r_dashboard_SelectNumForNumberOfDecline]
+END
+GO
+
+CREATE PROCEDURE [dbo].[pr_r_dashboard_SelectNumForNumberOfDecline]
+	-- Add the parameters for the stored procedure here
+	@realECD as varchar(10)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	Select Count(*) as Number From D_PurchReq											
+	Where RealECD = @realECD					
+	and (EndStatus = 2 or EndStatus = 3)
+	
+END
