798af3e86614663159219f68f596e11423eff556
Than Zaw Aung <capital.thanzawaung@gmail.com>
2022-05-09 15:41:36 +0630

    tza

---
+++ Models/RealEstate/r_com_profile/r_com_profileModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.RealEstate.r_com_profile
+{
+    public class r_com_profileModel
+    {
+    }
+}
---
+++ Seruichi.BL/RealEstate/r_com_profile/r_com_profileBL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using Seruichi.Common;
+using Models.RealEstate.r_com_profile;
+using System.Collections.Generic;
+using Models;
+using System.Linq;
+
+namespace Seruichi.BL.RealEstate.r_com_profile
+{
+    public class r_com_profileBL
+    {
+    }
+}
