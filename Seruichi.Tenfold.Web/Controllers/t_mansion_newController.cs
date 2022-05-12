2501dbb0fce0fefb889aa54b4268300b7ea9515e
capital-poeeisan <capital.poeeisan@gmail.com>
2022-05-12 08:32:45 +0630

    pes

---
+++ Seruichi.Tenfold.Web/Controllers/t_mansion_newController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace Seruichi.Tenfold.Web.Controllers
+{
+    public class t_mansion_newController : BaseController
+    {
+        public ActionResult Index()
+        {
+           
+            
+            return View();
+        }
+    }
+}
