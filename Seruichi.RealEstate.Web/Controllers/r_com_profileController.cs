bc33af56fb8103f9b37356d1444e8fa8745d90c2
Than Zaw Aung <capital.thanzawaung@gmail.com>
2022-05-12 20:10:15 +0630

    tza

--- Seruichi.RealEstate.Web/Controllers/r_com_profileController.cs
+++ Seruichi.RealEstate.Web/Controllers/r_com_profileController.cs
@@ -9,12 +9,24 @@ using Seruichi.BL.RealEstate.r_com_profile;
 
 namespace Seruichi.RealEstate.Web.Controllers
 {
-    public class r_com_profileController : Controller
+    public class r_com_profileController : BaseController
     {
         // GET: r_com_profile
         public ActionResult Index()
         {
             return View();
+        }
+
+        public r_com_profileModel GetLogin_Info()
+        {
+            r_com_profileModel model = new r_com_profileModel();
+            model.LoginID = base.GetOperator("UserID");
+            model.RealECD = base.GetOperator("RealECD");
+            model.PermissionChat = Convert.ToByte(base.GetOperator("PermissionChat"));
+            model.PermissionSetting = Convert.ToByte(base.GetOperator("PermissionSetting"));
+            model.PermissionPlan = Convert.ToByte(base.GetOperator("PermissionPlan"));
+            model.PermissionInvoice = Convert.ToByte(base.GetOperator("PermissionInvoice"));
+            return model;
         }
     }
 }
