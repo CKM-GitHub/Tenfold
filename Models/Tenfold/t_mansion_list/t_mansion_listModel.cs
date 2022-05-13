39e2681c203de7e4c5a8a8b8e11ebdfd306376f2
Pann Nu Zan <Pann Nu Zan@DESKTOP-52825H4>
2022-05-13 11:19:47 +0630

    pnz

--- Models/Tenfold/t_mansion_list/t_mansion_listModel.cs
+++ Models/Tenfold/t_mansion_list/t_mansion_listModel.cs
@@ -14,4 +14,18 @@ namespace Models.Tenfold.t_mansion_list
         public string StartUnit { get; set; }
         public string EndUnit { get; set; }
     }
+
+    public class t_mansion_list_l_log_Model
+    {
+        public string LogDateTime { get; set; }
+        public byte LoginKBN { get; set; }
+        public string LoginID { get; set; }
+        public string RealECD { get; set; }
+        public string LoginName { get; set; }
+        public string IPAddress { get; set; }
+        public string PageID { get; set; }
+        public string ProcessKBN { get; set; }
+        public string Remarks { get; set; }
+        public string MansionCD { get; set; }
+    }
 }
