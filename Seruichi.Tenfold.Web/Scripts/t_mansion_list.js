b35d569af4317978a9ad07b6a2d87cc05ec7b7d1
nwemar <capital.nwemarwin@gmail.com>
2022-05-16 17:11:43 +0630

    NMW

--- Seruichi.Tenfold.Web/Scripts/t_mansion_list.js
+++ Seruichi.Tenfold.Web/Scripts/t_mansion_list.js
@@ -242,6 +242,20 @@ function addEvents() {
         )
 
     });
+
+    $(document).on('click', '.tree li  input[type="checkbox"]', function () {
+        $(this).closest('li').find('ul input[type="checkbox"]').prop('checked', $(this).is(':checked'));
+    }).on('click', '.node-item', function () {
+        var parentNode = $(this).parents('.tree ul');
+        if ($(this).is(':checked')) {
+            parentNode.find('li .node-parent').prop('checked', true);
+        } else {
+            var elements = parentNode.find('ul input[type="checkbox"]:checked');
+            if (elements.length == 0) {
+                parentNode.find('li .node-parent').prop('checked', false);
+            }
+        }
+    });
 }
 
 function Get_FT_Age(age, type) {
