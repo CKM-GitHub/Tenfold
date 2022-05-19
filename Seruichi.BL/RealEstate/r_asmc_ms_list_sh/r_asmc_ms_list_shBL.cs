52f4c4bc758b2671e4af1a5d83c10571528a9b3a
Than Zaw Aung <capital.thanzawaung@gmail.com>
2022-05-19 16:41:35 +0630

    tza

---
+++ Seruichi.BL/RealEstate/r_asmc_ms_list_sh/r_asmc_ms_list_shBL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using Seruichi.Common;
+using Models.RealEstate.r_asmc_ms_list_sh;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace Seruichi.BL.RealEstate.r_asmc_ms_list_sh
+{
+    public class r_asmc_ms_list_shBL
+    {
+    }
+}
