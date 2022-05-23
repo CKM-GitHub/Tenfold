f557b10d47a5cc28369f13cf4adb91e1b7299cb5
capital-chochothae <capital.chochothae@gmail.com>
2022-05-23 12:45:41 +0630

    add by ct

--- Seruichi.RealEstate.Web/Content/scripts/tree.js
+++ Seruichi.RealEstate.Web/Content/scripts/tree.js
@@ -55,3 +55,52 @@
 
 $('#tree').treed({openedClass:'fa-minus', closedClass:'fa-plus'});
 
+
+//checkbox-indeterminate
+$(document).on('change','input[type="checkbox"]',function(e) {
+  var checked = $(this).prop("checked");
+  var container = $(this).parent();
+  var siblings = container.siblings();
+
+  container.find('input[type="checkbox"]').prop({
+    indeterminate: false,
+    checked: checked
+  });
+
+  function checkSiblings(el) {
+
+    var parent = el.parent().parent();
+    var all = true;
+
+    el.siblings().each(function() {
+      return all = ($(this).children('input[type="checkbox"]').prop("checked") === checked);
+    });
+
+    if (all && checked) {
+
+      parent.children('input[type="checkbox"]').prop({
+        indeterminate: false,
+        checked: checked
+      });
+
+      checkSiblings(parent);
+
+    } else if (all && !checked) {
+
+      parent.children('input[type="checkbox"]').prop("checked", checked);
+      parent.children('input[type="checkbox"]').prop("indeterminate", (parent.find('input[type="checkbox"]:checked').length > 0));
+      checkSiblings(parent);
+
+    } else {
+
+      el.parents("li").children('input[type="checkbox"]').prop({
+        indeterminate: true,
+        checked: false
+      });
+
+    }
+
+  }
+
+  checkSiblings(container);
+});
