ebcb4175d61ee25f3ad80a5a35a256333884ee97
Pann Nu Zan <Pann Nu Zan@DESKTOP-52825H4>
2022-05-26 11:17:18 +0630

    pnz

---
+++ Models/Tenfold/t_reale_list/t_reale_listModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Tenfold.t_reale_list
+{
+    public class t_reale_listModel
+    {
+        public string PrefNameSelect { get; set; }
+    }
+}
---
+++ Seruichi.Tenfold.Web/Controllers/t_reale_listController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Seruichi.Common;
+using Seruichi.BL.Tenfold.t_reale_list;
+
+namespace Seruichi.Tenfold.Web.Controllers
+{
+    public class t_reale_listController : BaseController
+    {
+        // GET: t_reale_list
+        public ActionResult Index()
+        {
+            t_reale_listBL bl = new t_reale_listBL();
+            ViewBag.PrefDropDownListItems = bl.GetDropDownListForPref();
+            return View();
+        }
+    }
+}
