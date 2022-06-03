f7de12f06c0e0d5e83191714e33c61c8528a4c6e
Pann Nu Zan <Pann Nu Zan@DESKTOP-52825H4>
2022-06-03 10:52:49 +0630

    pnz

---
+++ Seruichi.Seller.Web/Controllers/a_mypage_taikaiController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Seruichi.BL;
+using Seruichi.Common;
+using Models;
+using Seruichi.BL.Seller;
+using System.Data;
+
+
+namespace Seruichi.Seller.Web.Controllers
+{
+    public class a_mypage_taikaiController : BaseController
+    {
+        // GET: a_mypage_taikai
+        public ActionResult Index()
+        {
+            LoginUser user = SessionAuthenticationHelper.GetUserFromSession();
+            if (!SessionAuthenticationHelper.ValidateUser(user))
+            {
+                return RedirectToAction("Index", "a_login");
+            }
+            return View();
+        }
+    }
+}
