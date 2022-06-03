310f728a0205da2b72068649333f2a993a8d4650
Pann Nu Zan <Pann Nu Zan@DESKTOP-52825H4>
2022-06-03 11:36:09 +0630

    pnz

---
+++ Seruichi.BL/Seller/a_mypage_taikaiBL.cs
@@ -0,0 +1,32 @@
+﻿using Models;
+using Seruichi.Common;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web;
+
+namespace Seruichi.BL.Seller
+{
+
+    public class a_mypage_taikaiBL
+    {
+        public a_mypage_taikaiModel Get_Count(string UserID)
+        {
+            var sqlParms = new SqlParameter[]
+            {
+                new SqlParameter("@SellerCD",SqlDbType.VarChar){Value = UserID}
+            };
+
+            DBAccess db = new DBAccess();
+            var dt = db.SelectDatatable("pr_a_mypage_plus_Select_D_SellerPossible_bySellerCD", sqlParms);
+            a_mypage_taikaiModel model = DataTableExtentions.ToEntity<a_mypage_taikaiModel>(dt.Rows[0]);
+            return model;
+
+        }
+    }
+
+        
+       
+
+}
