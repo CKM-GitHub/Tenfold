8714bd7959e125c5b60d42b79685c1ba41a2a92b
capital-chochothae <capital.chochothae@gmail.com>
2022-06-06 00:51:10 +0630

    add by ct

---
+++ Seruichi.Tenfold.Web/Controllers/t_seller_accountController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Seruichi.Tenfold.Web.Controllers
+{
+    public class t_seller_accountController : Controller
+    {
+        // GET: t_seller_account
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
