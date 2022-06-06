acef2bb41ca035fce3d2850085511dcdc6e51e91
Than Zaw Aung <capital.thanzawaung@gmail.com>
2022-06-06 10:47:02 +0630

    tza

---
+++ Seruichi.BL/Tenfold/t_seller_memo/t_seller_memoBL.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using Seruichi.Common;
+using Models.Tenfold.t_seller_memo;
+
+namespace Seruichi.BL.Tenfold.t_seller_memo
+{
+    public class t_seller_memoBL
+    {
+    }
+}
---
+++ Seruichi.Tenfold.Web/Controllers/t_seller_memoController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Seruichi.Common;
+using Models.Tenfold.t_seller_memo;
+using Seruichi.BL.Tenfold.t_seller_memo;
+
+
+namespace Seruichi.Tenfold.Web.Controllers
+{
+    public class t_seller_memoController : Controller
+    {
+        // GET: t_seller_memo
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
