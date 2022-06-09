3a077d9d0b2868f66c9814a15c81ed4069e9be4d
Than Zaw Aung <capital.thanzawaung@gmail.com>
2022-06-09 15:40:39 +0630

    tza

---
+++ Seruichi.BL/Tenfold/t_reale_memo/t_reale_memoBL.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using Seruichi.Common;
+using Models.Tenfold.t_reale_memo;
+
+namespace Seruichi.BL.Tenfold.t_reale_memo
+{
+    public class t_reale_memoBL
+    {
+    }
+}
---
+++ Seruichi.Tenfold.Web/Controllers/t_reale_memoController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Seruichi.Common;
+using Models.Tenfold.t_reale_memo;
+using Seruichi.BL.Tenfold.t_reale_memo;
+
+namespace Seruichi.Tenfold.Web.Controllers
+{
+    public class t_reale_memoController : BaseController
+    {
+        // GET: t_reale_memo
+        public ActionResult Index(string RealECD)
+        {
+            t_reale_memoBL bl = new t_reale_memoBL();
+            //string sellername = bl.get_t_sellerName(RealECD);
+            //ViewBag.Title = "管理－売主－" + sellername;
+            ViewBag.LoginID = base.GetOperator();
+            return View();
+        }
+    }
+}
