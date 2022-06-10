6cf212a54fa0e5317010e48224e637f5c9c528fa
Pann Nu Zan <Pann Nu Zan@DESKTOP-52825H4>
2022-06-10 09:27:01 +0630

    pnz

--- Models/RealEstate/r_auto_mes/r_auto_mesModel.cs
+++ Models/RealEstate/r_auto_mes/r_auto_mesModel.cs
@@ -8,5 +8,12 @@ namespace Models.RealEstate.r_auto_mes
 {
     public class r_auto_mesModel
     {
+        public string RealECD { get; set; }
+        public string SEQ { get; set; }
+        public string MessageSEQ { get; set; }
+        public string MessageTitle { get; set; }
+        public string MessageTEXT { get; set; }
+        public string PermissionSetting { get; set; }
+        public string Applying { get; set; }
     }
 }
--- Seruichi.RealEstate.Web/Controllers/r_auto_mesController.cs
+++ Seruichi.RealEstate.Web/Controllers/r_auto_mesController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Models.RealEstate.r_login;
+using Seruichi.BL.RealEstate.r_auto_mes;
+using Models.RealEstate.r_auto_mes;
+using System.Data;
 
 namespace Seruichi.RealEstate.Web.Controllers
 {
@@ -11,6 +15,29 @@ namespace Seruichi.RealEstate.Web.Controllers
         // GET: r_auto_mes
         public ActionResult Index()
         {
+            
+            r_loginModel user = SessionAuthenticationHelper.GetUserFromSession();
+            if(!SessionAuthenticationHelper.ValidateUser(user))
+            {
+                return RedirectToAction("Index", "r_login");
+            }
+            r_auto_mesBL bl = new r_auto_mesBL();
+            List<r_auto_mesModel> MesModel = new List<r_auto_mesModel>();
+            DataTable dt = bl.Get_M_REMessage_By_RealECD(user.RealECD);
+            MesModel = (from DataRow dr in dt.Rows
+                        select new r_auto_mesModel()
+                        {
+                            RealECD = dr["RealECD"].ToString(),
+                            SEQ = dr["SEQ"].ToString(),
+                            MessageSEQ = dr["MessageSEQ"].ToString(),
+                            MessageTitle = dr["MessageTitle"].ToString(),                            
+                            MessageTEXT = dr["MessageTEXT"].ToString(),
+                            Applying = dr["Applying"].ToString()
+                        }).ToList();
+
+            ViewBag.AutoMsg = MesModel;
+            ViewBag.IsSetting = user.PermissionSetting;
+
             return View();
         }
     }
