7c41103ad2fab743e99c11a24e527c5ab8e9f3ee
Than Zaw Aung <capital.thanzawaung@gmail.com>
2022-06-13 09:06:17 +0630

    tza

---
+++ Seruichi.Tenfold.Web/Controllers/t_seller_newController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Seruichi.Common;
+using Models.Tenfold.t_seller_new;
+using Seruichi.BL.Tenfold.t_seller_new;
+
+namespace Seruichi.Tenfold.Web.Controllers
+{
+    public class t_seller_newController : BaseController
+    {
+        // GET: t_seller_new
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
