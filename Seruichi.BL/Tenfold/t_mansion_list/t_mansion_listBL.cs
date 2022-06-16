2f53103d4fa39d18b7b896e93a293c261acf1d93
Pann Nu Zan <Pann Nu Zan@DESKTOP-52825H4>
2022-06-16 14:23:10 +0630

    pnz

--- Seruichi.BL/Tenfold/t_mansion_list/t_mansion_listBL.cs
+++ Seruichi.BL/Tenfold/t_mansion_list/t_mansion_listBL.cs
@@ -125,8 +125,8 @@ namespace Seruichi.BL.Tenfold.t_mansion_list
         {
             ValidatorAllItems validator = new ValidatorAllItems();
 
-            validator.CheckIsHalfWidth("StartNum", model.StartAge,2, RegexFormat.Number); //E104,E105 
-            validator.CheckIsHalfWidth("EndNum", model.EndAge, 2, RegexFormat.Number); //E104,E105 
+            validator.CheckIsHalfWidth("StartNum", model.StartAge,7, RegexFormat.Number); //E104,E105 
+            validator.CheckIsHalfWidth("EndNum", model.EndAge, 7, RegexFormat.Number); //E104,E105 
 
             validator.CheckIsHalfWidth("StartUnit", model.StartUnit, 3, RegexFormat.Number); //E104,E105 
             validator.CheckIsHalfWidth("EndUnit", model.EndUnit, 3, RegexFormat.Number); //E104,E105 
