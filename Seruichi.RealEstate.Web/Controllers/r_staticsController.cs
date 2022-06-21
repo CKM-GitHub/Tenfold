b2fa0ed001e51c231e3080f33c2a7384f78ca9ce
Than Zaw Aung <capital.thanzawaung@gmail.com>
2022-06-21 16:06:07 +0630

    tza

---
+++ Seruichi.RealEstate.Web/Controllers/r_staticsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Models.RealEstate.r_statics;
+using Seruichi.BL;
+using Seruichi.BL.RealEstate.r_statics;
+
+namespace Seruichi.RealEstate.Web.Controllers
+{
+    public class r_staticsController : BaseController
+    {
+        // GET: r_statics
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
