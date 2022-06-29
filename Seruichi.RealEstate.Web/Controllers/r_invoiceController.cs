09b8967b021388316d289dac068fc7303d8f53b6
capital-chochothae <capital.chochothae@gmail.com>
2022-06-29 09:50:27 +0630

    cho thae

--- Seruichi.RealEstate.Web/Controllers/r_invoiceController.cs
+++ Seruichi.RealEstate.Web/Controllers/r_invoiceController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Models.RealEstate.r_invoice;
+using Seruichi.BL.RealEstate.r_invoice;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,12 +8,25 @@ using System.Web.Mvc;
 
 namespace Seruichi.RealEstate.Web.Controllers
 {
-    public class r_invoiceController : Controller
+    public class r_invoiceController : BaseController
     {
         // GET: r_invoice
         public ActionResult Index()
         {
             return View();
         }
+
+        ////[HttpPost]
+        ////public ActionResult Get_D_Billing_List(r_invoiceModel model)
+        ////{
+        ////    r_invoiceBL bl = new r_invoiceBL();
+        ////    var validationResult = bl.ValidateAll(model);
+        ////    if (validationResult.Count > 0)
+        ////    {
+        ////        return ErrorResult(validationResult);
+        ////    }
+        ////    var dt = bl.Get_D_Billing_List(model);
+        ////    return OKResult(DataTableToJSON(dt));
+        ////}
     }
 }
