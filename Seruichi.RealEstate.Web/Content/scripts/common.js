3f4c174fe11177496bdda9a0f84af8e29a5bd3a5
capital-chochothae <capital.chochothae@gmail.com>
2022-07-11 14:06:14 +0630

    cho thae

--- Seruichi.RealEstate.Web/Content/scripts/common.js
+++ Seruichi.RealEstate.Web/Content/scripts/common.js
@@ -634,7 +634,7 @@ const common = {
             }
             if (isemailformat) {
                 if (!inputValue.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
-                    $ctrl.showError(this.getMessage('E104'));
+                    $ctrl.showError(this.getMessage('E204'));
                     return;
                 }
             }
